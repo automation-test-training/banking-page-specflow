3c915623853c6ecd04f5804ddf89960abcd70c27
qinyu <qinyusuain@gmail.com>
2016-09-04 14:44:17 +0800

    create account feature

---
+++ banking-page-specflow/AccountFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: Show Accounts and Balance
+
+  Scenario: Show multiple accounts
+    Given a user has 2 accounts
+    When I refresh account
+    Then I should see accounts and balances:
+      | account | cny balance  | usd balance |
+      | 1001    | - cny 100000 | - usd 30000 |
+      | 2001    | - cny 200000 |             |
---
+++ banking-page-specflow/AccountSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace banking_page_specflow
+{
+    [Binding]
+    public class ShowAccountsAndBalanceSteps
+    {
+        [Given(@"a user has (.*) accounts")]
+        public void GivenAUserHasAccounts(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I refresh account")]
+        public void WhenIRefreshAccount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see accounts and balances:")]
+        public void ThenIShouldSeeAccountsAndBalances(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
